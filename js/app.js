59a5b25e64e6dfc27f024cb20cb4184e12653017
designer-mehedi <mehedihasan80912@gmail.com>
2022-09-04 01:24:50 +0600

    responsive pages

--- js/app.js
+++ js/app.js
@@ -65,34 +65,43 @@ const displayNewsPosts = newsPosts => {
         const postDiv = document.createElement('div');
         postDiv.classList.add('col')
         postDiv.innerHTML = `
-            <div class="card mb-3 border-0">
-                        <div class="row">
-                            <div class="col-md-3 py-3 ps-4">
-                                <img src="${posts.thumbnail_url}" class="img-fluid rounded thumb-img" alt="...">
-                            </div>
-                            <div class="col-md-9">
-                                <div class="card-body">
-                                    <h4 class="card-title">${posts.title}</h4>
-                                    <p class="card-text">${posts.details}</p>
-                                    
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row px-4 pb-3">
-                                        <div class="d-flex justify-content-between align-items-center mt-5">
-                                        <div class="author-detail d-flex align-items-center">
-                                            <div class="image"><img class="post-img" src="${posts.author.img}"></div>
-                                            <div class="text-content d-flex flex-column ms-2">
-                                                <span>${posts.author.name ? posts.author.name : "No Authors Found"}</span> 
-                                                <span class="">${posts.author.published_date}</span>
-                                            </div>
-                                        </div>
-                                        <p>${posts.total_view ? posts.total_view : 'No views found'}</p>
-                                        <p>${posts.rating.number} ${posts.rating.badge}</p>
-                                        <button onclick="loadNewsDetails('${posts._id}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newsDetailModal">Details</button>
-                                    </div>
-                        </div>
+            <div class="card mb-3 border-0 pb-4">
+        <div class="row">
+            <div class="col-md-3 py-3 ps-4">
+                <img src="${posts.thumbnail_url}" class="img-fluid rounded thumb-img" alt="...">
+            </div>
+            <div class="col-md-9">
+                <div class="card-body">
+                    <h4 class="card-title">${posts.title}</h4>
+                    <p class="card-text card-style">${posts.details}</p>
+                </div>
+            </div>
+        </div>
+        <div class="row px-4 g-5 pt-2">
+            <div class="col-md-7 author-detail d-flex align-items-center justify-content-between">
+                    <div class="image"><img class="post-img" src="${posts.author.img}"></div>
+                    <div class="text-content d-flex flex-column ms-2">
+                        <span>${posts.author.name ? posts.author.name : "No Authors Found"}</span>
+                        <span class="">${posts.author.published_date}</span>
                     </div>
+                    <div class="d-flex align-items-center">
+                        <span class="icon"><ion-icon name="eye"></ion-icon> </span>
+                        <p class="pt-2">${posts.total_view ? posts.total_view : 'No views found'}</p>
+                    </div>
+                </div>
+                <div class="col-md-5 d-flex align-items-center justify-content-between">
+                   <div>
+                    <ion-icon name="star"></ion-icon>
+                    <ion-icon name="star"></ion-icon>
+                    <ion-icon name="star"></ion-icon>
+                    <ion-icon name="star-half"></ion-icon>
+                    <ion-icon name="star-outline"></ion-icon>
+                   </div>
+                    <button onclick="loadNewsDetails('${posts._id}')" class="btn btn-primary" data-bs-toggle="modal"
+                        data-bs-target="#newsDetailModal">Details</button>
+                </div>
+            </div>
+         </div>
         `; 
         singleNewsCategory.appendChild(postDiv);
     })
